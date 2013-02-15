8a8cfa4c6e298e97f0818f0b2fdf2a62bc3fc6c2
jdlx <github@rexdev.de>
2013-02-15 10:40:13 +0100

    version bump -> 1.1.62

--- config.inc.php
+++ config.inc.php
@@ -7,7 +7,7 @@
  * @author Markitup by Jay Salvat - http://markitup.jaysalvat.com
  * @author Redaxo Addon by http://www.gn2-netwerk.de/
  * @package redaxo 4.2/4.3/4.4
- * @version 1.1.61
+ * @version 1.1.62
  */
 
 
@@ -22,7 +22,7 @@ $REX['ADDON'][$myself]['VERSION'] = array
 (
 'VERSION'      => 1,
 'MINORVERSION' => 1,
-'SUBVERSION'   => 61
+'SUBVERSION'   => 62
 );
 
 // ADDON REX COMMONS
